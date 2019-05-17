598c2425daeb61bf89979dec3d6a07312d72c924
lfitchett <lfitchett1@gmail.com>
2019-05-17 12:37:50 -0500

    initial commit

--- GameEngineAllegro/GameEngineAllegro.cpp
+++ GameEngineAllegro/GameEngineAllegro.cpp
@@ -1,21 +1,17 @@
-// GameEngineAllegro.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
-#include <iostream>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	al_init();
+	al_init_font_addon();
+	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+	ALLEGRO_FONT* font = al_create_builtin_font();
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_draw_text(font, al_map_rgb(255, 255, 255), 400, 300, ALLEGRO_ALIGN_CENTER, "Welcome to Allegro!");
+	al_flip_display();
+	al_rest(5.0);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
