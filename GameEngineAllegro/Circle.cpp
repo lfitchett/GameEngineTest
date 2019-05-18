8867d3077c2deae35082751f4f9cca4ff8592ab4
lfitchett <lfitchett1@gmail.com>
2019-05-17 21:07:14 -0500

    draw box

--- GameEngineAllegro/Circle.cpp
+++ GameEngineAllegro/Circle.cpp
@@ -7,20 +7,33 @@ class Circle : public EntityWithData
 public:
 	Circle(EventLoop &loop, SharedData &data) : EntityWithData(loop, data)
 	{
-		printf("Making Circle\n");
+		bitmap = al_create_bitmap(20, 20);
+		if (!bitmap) {
+			printf("Failed to make bitmap\n");
+		}
+
+		al_set_target_bitmap(bitmap);
+		al_clear_to_color(al_map_rgb(255, 0, 255));
+		al_set_target_bitmap(al_get_backbuffer(sharedData.display));
+
+		currLocation.x = 400;
+		currLocation.y = 300;
 	}
 
 	~Circle() 
 	{
+		al_destroy_bitmap(bitmap);
 		printf("Cleanup Circle\n");
 	}
 
 private:
 	Location currLocation;
+	ALLEGRO_BITMAP* bitmap;
 
 protected:
 	void Tick()
 	{
+		al_draw_bitmap(bitmap, currLocation.x, currLocation.y, 0);
 		sharedData.mouseLocation.print();
 	}
 };
